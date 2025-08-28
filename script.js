879e1bc8255e22d8b8e7d41e8ec0ba548af272b2
Geninho <genisson465@gmail.com>
2025-08-28 20:56:30 -0300

    up nav

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const checkbox = document.getElementById('chk');
 const ball = document.querySelector('.ball');
+const profileImg = document.getElementById('profile-img'); // pega a foto principal
 
 checkbox.addEventListener('change', () => {
   ball.style.transform = checkbox.checked ? 'translateX(32px)' : 'translateX(0)';
   document.body.classList.toggle('dark-mode', checkbox.checked);
-});
 
+  // troca a foto no modo dark/light
+  if (checkbox.checked) {
+    profileImg.src = "assets/profile-dark.png"; // imagem para dark mode
+  } else {
+    profileImg.src = "assets/profile-light1.png"; // imagem para light mode
+  }
+});
 // Adiciona/remover a classe .hovered ao tocar/clicar no mobile
 function enableTouchHover(selector, hoverClass = "hovered") {
   document.querySelectorAll(selector).forEach(el => {
